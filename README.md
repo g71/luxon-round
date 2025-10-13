5d18c860e43b0ee27bf2fc7e381f9a2bf37d0a93
Gabriel Åhbeck <gabriel.ahbeck@gmail.com>
2025-10-13 20:36:28 +0200

    ex nihilo

---
+++ README.md
@@ -0,0 +1,12 @@
+# luxon-round
+
+Add floor, ceil, and round to multiple of Luxon DateTime units.
+
+    // round to closest 15 minutes
+    DateTime.fromISO("2025-10-03T11:47:11.014+02:00").round('minute', 15) // "2025-10-03T11:45:00.000+02:00"
+
+    // floor to start of quarter
+    DateTime.fromISO("2025-08-03T16:59:12.014+02:00").floor('month', 3) // "2025-06-01T00:00:00.000+02:00"
+    
+    // ceil to next year (equivalent to DateTime.startOf('year').plus({year: 1});
+    DateTime.fromISO("2025-08-03T16:59:12.014+02:00").ceil('year', 1) // "2026-01-01T00:00:00.000+02:00"
---
+++ luxon-round.js
@@ -0,0 +1,35 @@
+import { DateTime } from 'luxon';
+
+// Ceil/floor/round to multiple of unit
+function adjustTime(adjust, dateTime, unit, multiple) {
+  const unitValue = dateTime.get(unit);
+  const raw = unitValue / multiple;
+  const adjusted = adjust(raw);
+  const direction = adjust === Math.ceil || adjusted > raw ? 1 : 0;
+  const startOfUnit = dateTime.startOf(unit);
+  // increase unit value if rounding up and sub-units are > 0
+  const calcValue = unitValue + (dateTime > startOfUnit ? direction : 0);
+
+  const newValue = {};
+  newValue[unit] = adjust(calcValue / multiple) * multiple;
+
+  return startOfUnit.set(newValue);
+}
+
+if (!DateTime.prototype.floor) {
+  DateTime.prototype.floor = function(unit, multiple) {
+    return adjustTime(Math.floor, this, unit, multiple);
+  }
+}
+
+if (!DateTime.prototype.round) {
+  DateTime.prototype.round = function(unit, multiple) {
+    return adjustTime(Math.round, this, unit, multiple);
+  }
+}
+
+if (!DateTime.prototype.ceil) {
+  DateTime.prototype.ceil = function(unit, multiple) {
+    return adjustTime(Math.ceil, this, unit, multiple);
+  }
+}
